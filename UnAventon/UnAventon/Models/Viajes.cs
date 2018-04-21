defa296ecc77d1887ab8812f3905d7a28118c3d3
joselicht90 <jose.licht@hotmail.com>
2018-04-21 16:47:41 -0300

    NHibernate primeros pasos

---
+++ UnAventon/UnAventon/Models/Viajes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnAventon.Models
+{
+    public class Viajes
+    {
+        public Viajes() { }
+        public virtual long Id { get; set; }
+        public virtual decimal Precio { get; set; }
+        public virtual DateTime FechaViaje { get; set; }
+        public virtual DateTime FechaBaja { get; set; }
+        public virtual DateTime FechaAlta { get; set; }
+        public virtual long NViaje { get; set; }
+        public virtual long Periodico  { get; set; }
+        public virtual long CalificacionConductor { get; set; }
+    }
+}
---
+++ UnAventon/UnAventon/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace QuickStart
+{
+    public sealed class NHibernateHelper
+    {
+        private const string CurrentSessionKey = "nhibernate.current_session";
+        private static readonly ISessionFactory _sessionFactory;
+
+        static NHibernateHelper()
+        {
+            _sessionFactory = new Configuration().Configure().BuildSessionFactory();
+        }
+
+        public static ISession GetCurrentSession()
+        {
+            var context = HttpContext.Current;
+            var currentSession = context.Items[CurrentSessionKey] as ISession;
+
+            if (currentSession == null)
+            {
+                currentSession = _sessionFactory.OpenSession();
+                context.Items[CurrentSessionKey] = currentSession;
+            }
+
+            return currentSession;
+        }
+
+        public static void CloseSession()
+        {
+            var context = HttpContext.Current;
+            var currentSession = context.Items[CurrentSessionKey] as ISession;
+
+            if (currentSession == null)
+            {
+                // No current session
+                return;
+            }
+
+            currentSession.Close();
+            context.Items.Remove(CurrentSessionKey);
+        }
+
+        public static void CloseSessionFactory()
+        {
+            if (_sessionFactory != null)
+            {
+                _sessionFactory.Close();
+            }
+        }
+    }
+}
