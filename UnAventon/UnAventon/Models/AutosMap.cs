45e9997b7707c4d846e683383b025342cdd8c409
joselicht90 <36008070+joselicht90@users.noreply.github.com>
2018-05-10 18:26:55 -0300

    adfasf

--- UnAventon/UnAventon/Models/AutosMap.cs
+++ UnAventon/UnAventon/Models/AutosMap.cs
@@ -19,19 +19,3 @@ namespace UnAventon.Models
         }
     }
 }
-
-
-namespace UnAventon.Models
-{
-    public class CiudadesMap : ClassMap<Ciudades>
-    {
-        public CiudadesMap()
-        {
-            Table("UA_CIUDADES");
-            Id(x => x.Id, "ID").GeneratedBy.Native();
-            Map(x => x.Nombre, "NOMBRE").Not.Nullable();
-            Map(x => x.Cp, "CP").Not.Nullable();
-        }
-
-    }
-}
