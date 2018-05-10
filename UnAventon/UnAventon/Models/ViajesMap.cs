5a0b4d1d81a3950b7e1406752c286f6ac5be3808
joselicht90 <36008070+joselicht90@users.noreply.github.com>
2018-05-10 16:17:35 -0300

    sadf

---
+++ UnAventon/UnAventon/Models/ViajesMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnAventon.Models
+{
+    public class ViajesMap: ClassMap<Viajes>
+    {
+        public ViajesMap()
+        {
+            Table("UA_VIAJES");
+            Id(x => x.Id, "ID").GeneratedBy.Native();
+            Map(x => x.FechaAlta, "FECHA_ALTA").Not.Nullable();
+        }
+    }
+}
