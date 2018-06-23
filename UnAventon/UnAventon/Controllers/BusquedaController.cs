cb365a0ee1845b38030391f9e54d206a8b5dfa8b
joselicht90 <36008070+joselicht90@users.noreply.github.com>
2018-06-23 12:13:03 -0300

    ALmost Done

--- UnAventon/UnAventon/Controllers/BusquedaController.cs
+++ UnAventon/UnAventon/Controllers/BusquedaController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using NHibernate;
+using NHibernate.Criterion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UnAventon.Models;
+using UnAventon.NHibernate;
 
 namespace UnAventon.Controllers
 {
@@ -13,6 +17,23 @@ namespace UnAventon.Controllers
         {
 
             return View();
+        }
+        public ActionResult BuscarViajes(string fechaDesde, string fechaHasta, long idOrigen, long idDestino)
+        {
+            DateTime fechaInicio = DateTime.Parse(fechaDesde);
+            DateTime fechaFin = DateTime.Parse(fechaHasta).AddHours(23).AddMinutes(59).AddSeconds(59);
+            ISession session = NHibernateHelper.GetCurrentSession();
+            ICriteria criteria = session.CreateCriteria<Viajes>();
+            criteria
+                .CreateAlias("Origen","Origen")
+                .CreateAlias("Destino","Destino")
+                .Add(Expression.Ge("FechaViaje", fechaInicio))
+                .Add(Expression.Le("FechaViaje", fechaFin))
+                .Add(Expression.Eq("Origen.Id", idOrigen))
+                .Add(Expression.Eq("Destino.Id", idDestino));
+            List<Viajes> viajes = criteria.List<Viajes>().ToList();
+            ViewBag.Viajes = viajes;
+            return PartialView("_ListadoViajesBusqueda");
         }
     }
 }
